45552be8104b7d265c0d1dd0521d71a8a872a39d
Gennadii Shpreer <intellegento@gmail.com>
2023-04-14 17:22:21 +0600

    feat: resolved 1 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((acc, arr) => {
+    const catEars = arr.filter(el => el === '^^').length;
+    return acc + catEars;
+  }, 0);
 }
 
 module.exports = {
